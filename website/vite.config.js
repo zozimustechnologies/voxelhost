b3d2334e4c36fcdc285fb27e9fab5dfa64d23d82
Avi Chadda <chaddaavi@gmail.com>
2026-07-25 22:28:36 +0530

    ci: add GitHub Pages deploy workflow, set base path /voxelhost/

--- website/vite.config.js
+++ website/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/voxelhost/',
   server: {
     port: 3000,
     strictPort: true,
